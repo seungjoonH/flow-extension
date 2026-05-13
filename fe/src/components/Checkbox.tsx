b689b60c59284ade4c1074fe0e28e4636b881498
seungjoonH <hsj6831@gmail.com>
2026-05-14 01:02:45 +0900

    refactor: UI 컴포넌트 스타일 개선 및 구조 조정

--- fe/src/components/Checkbox.tsx
+++ fe/src/components/Checkbox.tsx
@@ -10,10 +10,10 @@ function Checkbox({ label, checked, onChange }: CheckboxProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.checked);
 
   return (
-    <div className={styles.checkbox}>
-      <input type="checkbox" checked={checked} onChange={handleChange} id={label} />
-      <label className={styles.label} htmlFor={label}>{label}</label>
-    </div>
+    <label className={styles.checkbox}>
+      <input type="checkbox" checked={checked} onChange={handleChange} />
+      <span className={styles.label}>{label}</span>
+    </label>
   );
 }
 
